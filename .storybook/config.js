330a8ba0adb11485592dc432dcdab7580193f9c0
Lior Frenkel <frenkeldev@gmail.com>
2019-01-29 14:23:57 +0100

    Add storybook addons

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,7 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs/react';
+
+addDecorator(withKnobs);
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.js$/);
